738a149d7a92f28413d966bcc0458b3e74148900
Ahmed Elsawalhy <mail@yagasoft.com>
2021-12-12 22:39:18 +0300

    Improved: extracted helpers into Common library. Fixed: loop stops working on page change.

--- Yagasoft.NotificationCentre.WebResources/JavaScript/ldv_NotificationsCentreRibbon.js
+++ Yagasoft.NotificationCentre.WebResources/JavaScript/ldv_NotificationsCentreRibbon.js
@@ -13,21 +13,7 @@ async function InitNotificationsMenu()
 			return false;
 		}
 
-		NcParent.NcLibrary = window;
-
-		for (let i = 0; i < 100; i++)
-		{
-			if (window.IsCommonGenericLibraryLoaded)
-			{
-				break;
-			}
-
-			console.warn('[NC] Waiting 50ms for the Common Library to load ...');
-			await new Promise(resolve => setTimeout(resolve, 50));
-		}
-
-		LoadWebResources(['ldv_NotificationsCentreLibsJs', 'ys_NotificationsCentreCore'], null, NcParent);
-		LoadWebResourceCss('ldv_NotificationsCentreCss', NcParent);
+		LoadWebResources('ys_NotificationsCentreCore', null, NcParent);
 	}
 	catch (e)
 	{
@@ -36,4 +22,76 @@ async function InitNotificationsMenu()
 	}
 
 	return false;
+}
+
+function LoadWebResources(resources, callback, scopeWindow)
+{
+	/// <summary>
+	///     Takes an array of resource names and loads them into the current context using "LoadScript".<br />
+	///     The resources param accepts a string as well in case a single resource is needed instead.<br />
+	///     Author: Ahmed Elsawalhy
+	/// </summary>
+	/// <param name="resources" type="String[] | string" optional="false">The resource[s] to load.</param>
+	/// <param name="callback" type="Function" optional="true">A function to call after resource[s] has been loaded.</param>
+	if (resources.length <= 0)
+	{
+		if (callback)
+		{
+			callback();
+		}
+
+		return;
+	}
+
+	if (typeof resources === 'string')
+	{
+		resources = [resources];
+	}
+
+	var localCallback = function()
+	{
+		if (resources.length > 1)
+		{
+			LoadWebResources(resources.slice(1, resources.length), callback, scopeWindow);
+		}
+		else
+		{
+			if (callback)
+			{
+				callback();
+			}
+		}
+	};
+
+	LoadScript(Xrm.Utility.getGlobalContext().getClientUrl() + '/WebResources/' + resources[0], localCallback, scopeWindow);
+}
+
+function LoadScript(url, callback, scopeWindow)
+{
+	/// <summary>
+	///     Takes a URL of a script file and loads it into the current context, and then calls the function passed.<br />
+	///     Author: Ahmed Elsawalhy<br />
+	///     credit: http://stackoverflow.com/a/950146/1919456
+	/// </summary>
+	/// <param name="url" type="String" optional="false">The URL to the script file.</param>
+	/// <param name="callback" type="Function" optional="true">The function to call after loading the script.</param>
+
+	scopeWindow = scopeWindow || window;
+	// Adding the script tag to the head as suggested before
+	var head = scopeWindow.document.getElementsByTagName('head')[0];
+	var script = scopeWindow.document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = url;
+
+	// Then bind the event to the callback function.
+	// There are several events for cross browser compatibility.
+
+	if (callback)
+	{
+		//script.onreadystatechange = callback;
+		script.onload = callback;
+	}
+
+	// Fire the loading
+	head.appendChild(script);
 }
